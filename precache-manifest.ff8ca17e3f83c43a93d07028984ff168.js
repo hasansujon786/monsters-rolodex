10a67e90352670ddf9ca4cd98803ab7f59f1a761
Hasan Mahmud <hasansujon786@gmail.com>
2019-09-04 08:39:37 -0700

    Updates

---
+++ precache-manifest.ff8ca17e3f83c43a93d07028984ff168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23935be2c2ad8f89a115c6de9aa3fde9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef58be16376efeed5c9a",
+    "url": "/static/css/main.0b124416.chunk.css"
+  },
+  {
+    "revision": "f1a8ddf31ee4c84e0a44",
+    "url": "/static/js/2.845eee23.chunk.js"
+  },
+  {
+    "revision": "ef58be16376efeed5c9a",
+    "url": "/static/js/main.28f1bd8a.chunk.js"
+  },
+  {
+    "revision": "41805a161343e8125279",
+    "url": "/static/js/runtime~main.cdd04e8b.js"
+  }
+]);
